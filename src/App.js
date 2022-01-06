0183e847a17829973b9f7b1b851aa8a51aec8325
Raiyan Afiz <raiyan.afiz@gmail.com>
2022-01-06 14:54:42 -0500

    Adds Images

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import {useState} from 'react'
 import Opening from './components/Opening';
 import Projects from './components/Projects';
 
+//Import Media
+import pic1 from './media/GitProfile1.jpg';
+import pic2 from './media/GitProfile2.jpg';
+import pic3 from './media/GitProfile3.jpg';
+
 const App = () => {
   const [projs] = useState([
     {
         id: 1,
         Title: "Project Name",
-        Picture: "Picture",
+        Picture: pic1,
         Description: "The description",
         Skills: "Skills Used",
         Link: "Link to GitHub"
@@ -17,7 +22,7 @@ const App = () => {
     {
         id: 2,
         Title: "Project Name",
-        Picture: "Picture",
+        Picture: pic2,
         Description: "The description",
         Skills: "Skills Used",
         Link: "Link to GitHub"
@@ -26,7 +31,7 @@ const App = () => {
     {
         id: 3,
         Title: "Project Name",
-        Picture: "Picture",
+        Picture: pic3,
         Description: "The description",
         Skills: "Skills Used",
         Link: "Link to GitHub"
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,7 +2,7 @@ const Project = ({proj}) => {
     return (
         <div className="project">
             <p>{proj.id}</p>
-            <p>{proj.Picture}</p>
+            <img src={proj.Picture} alt="My Git Profile" width="480" height="222"/>
             <p>{proj.Description}</p>
             <p>{proj.Skills}</p>
             <p>{proj.Link}</p>
