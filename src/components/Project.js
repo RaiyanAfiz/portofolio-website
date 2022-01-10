ee70ba5746c146a967db54e3b45c1a6b753bf756
Raiyan Afiz <raiyan.afiz@gmail.com>
2022-01-10 17:44:03 -0500

    Changed spacing

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,13 @@
 const Project = ({proj}) => {
     return (
         <div className="project">
+            <br/>
             <div id="project-styles">
                 <img src={proj.Picture} alt="My Git Profile" id="project-picture" width="480" height="222"/>
                 <p id="project-description">{proj.Description}</p>
                 <p id="project-skills">{proj.Skills}</p>
                 <p id="project-links">{proj.Link}</p>
             </div>
-            <br/>
             <br/>
         </div>
     )
