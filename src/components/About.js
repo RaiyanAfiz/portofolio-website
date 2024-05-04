e23460929ddce7ff450853ab5640401870c974c3
Raiyan Afiz <raiyan.afiz@gmail.com>
2024-05-03 20:49:07 -0400

    Updates About me

--- src/components/About.js
+++ src/components/About.js
@@ -3,17 +3,31 @@ const About = () => {
         <div>
             <br/><br/><br/>
             <h1 id="about-title">About Me</h1>
+            
             <p id="about-body">
-                Hello, I am currently a fourth year student studying computer science at the University of Windsor. 
-                I enjoy walking on trails, reading books, and learning weird but interesting thing.
+                Let me tell you three facts about myself. I love a good story, learning, and solving problems. If you deduced that I 
+                enjoy video games, you would be right, but you would be surprised at how well my interests align with my career.
             </p>
 
             <p id="about-body">
-                I previously worked as a Junior Digital Marketing Strategist, where my main job was to understand 
-                which factors caused certain ads to succeed, and which factor caused it to fail. <br/>
-                I am current working as a Business Intelligence Analyst. Where I use data to produce finance or market intelligence 
-                reports.
+                I have been a Business Intelligence Analyst for 16 months and in that time, I learned an array of skills but most 
+                importantly I discovered that data can tell a story, and then learned how to uncover that story. As a BI analyst my 
+                main job was to help my stakeholders see the story. To do that, I transformed the data and made it more digestible and 
+                accessible.
             </p>
+
+            <p id="about-body">
+                I am passionate about all things related to tech, so involving myself in these topics never gets tiring for me. 
+                My time in university helped me understand the importance of immersive learning, which in my case is starting a new 
+                software development project. It is a great way to improve my abilities and get hands-on experience with new technologies. 
+                As any developer would tell you, unexpected problems will always arise when working on a project, and I use this as an 
+                opportunity to apply my problem-solving skills.
+            </p>
+
+            <p id="about-body">
+                Look below to see some of my projects.
+            </p>
+
             <br/><br/><br/>
         </div>
     )
