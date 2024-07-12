94228ecb227614e3be3396c137eb0670540e95ea
Raiyan Afiz <raiyan.afiz@gmail.com>
2024-07-12 01:05:17 -0400

    Adds new project

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -1,53 +1,66 @@
 export const myProjects = [
+  {
+    id: 0,
+    title: "Java Tic-Tac-Toe",
+    picture: "/pictures/JavaTicTacToe.JPG",
+    pictureAlt: "A picture of the game in action.",
+    description:
+      "A CLI Java based tic-tac-toe game. The opponent AI makes decisions based on game state.",
+    skills: ["Java"],
+    link: "https://github.com/RaiyanAfiz/JavaTicTacToe",
+  },
 
-    {
-      id: 0,
-      title: "Java Tic-Tac-Toe",
-      picture: "/pictures/JavaTicTacToe.JPG",
-      pictureAlt: "A picture of the game in action.",
-      description: "A CLI Java based tic-tac-toe game. The opponent AI makes decisions based on game state.",
-      skills: ["Java"],
-      link: "https://github.com/RaiyanAfiz/JavaTicTacToe"
-    },
+  {
+    id: 1,
+    title: "Bookstore Website",
+    picture: "pictures/BookStoreWebsite.JPG",
+    pictureAlt: "A picture of the website",
+    description:
+      "A very simple website that uses HTML and CSS for the front end, and PHP for the backend. This mimics very simple bookstore and reading platform. Uses copyright free books.",
+    skills: ["PHP", "HTML", "CSS"],
+    link: "https://github.com/RaiyanAfiz/Website",
+  },
 
-    {
-        id: 1,
-        title: "Bookstore Website",
-        picture: "pictures/BookStoreWebsite.JPG",
-        pictureAlt: "A picture of the website",
-        description: "A very simple website that uses HTML and CSS for the front end, and PHP for the backend. This mimics very simple bookstore and reading platform. Uses copyright free books.",
-        skills: ["PHP", "HTML", "CSS"],
-        link: "https://github.com/RaiyanAfiz/Website"
-    },
+  {
+    id: 2,
+    title: "City of Windsor - Open Data",
+    picture: "pictures/OpenDataWebsite.JPG",
+    pictureAlt: "A picture of the website",
+    description:
+      "A website that takes data from the city of Windsor, which are usually in CSV or Excel formats, and displays the information on the website. The data in the website is in a JSON format.",
+    skills: ["HTML", "CSS", "JSON"],
+    link: "https://github.com/sell50/City-of-Windsor-Open-Data",
+  },
 
-    {
-        id: 2,
-        title: "City of Windsor - Open Data",
-        picture: "pictures/OpenDataWebsite.JPG",
-        pictureAlt: "A picture of the website",
-        description: "A website that takes data from the city of Windsor, which are usually in CSV or Excel formats, and displays the information on the website. The data in the website is in a JSON format.",
-        skills: ["HTML", "CSS", "JSON"],
-        link: "https://github.com/sell50/City-of-Windsor-Open-Data"
-    },
+  {
+    id: 3,
+    title: "CTF - Four Player Co-Op",
+    picture: "pictures/CTF-Game.JPG",
+    pictureAlt: "A picture of the game in action",
+    description:
+      "A Unity3D game featuring four player capture the flag game mode.",
+    skills: ["Unity", "C#"],
+    link: "https://github.com/sell50/GameDesignCourse",
+  },
 
-    {
-        id: 3,
-        title: "CTF - Four Player Co-Op",
-        picture: "pictures/CTF-Game.JPG",
-        pictureAlt: "A picture of the game in action",
-        description: "A Unity3D game featuring four player capture the flag game mode.",
-        skills: ["Unity", "C#"],
-        link: "https://github.com/sell50/GameDesignCourse"
-    },
+  {
+    id: 4,
+    title: "Mental Health Android App",
+    picture: "pictures/MentalHealthApp.JPG",
+    pictureAlt: "A picture of the app in action.",
+    description:
+      "An android app focusing on mental health. It uses RoomDB to track journal entries.",
+    skills: ["Android Studio", "Java", "SQLite", "RoomDB"],
+    link: "https://github.com/RaiyanAfiz/Mobile-Application-Development-Project",
+  },
 
-    {
-        id: 4,
-        title: "Mental Health Android App",
-        picture: "pictures/MentalHealthApp.JPG",
-        pictureAlt: "A picture of the app in action.",
-        description: "An android app focusing on mental health. It uses RoomDB to track journal entries.",
-        skills: ["Android Studio", "Java", "SQLite", "RoomDB"],
-        link: "https://github.com/RaiyanAfiz/Mobile-Application-Development-Project"
-    }
-
-]
+  {
+    id: 5,
+    title: "Toronto Open Data",
+    picture: "pictures/TorontoOpenData.JPG",
+    pictureAlt: "Database Table",
+    description: "A project focusing on ELT using Python and Pandas library",
+    skills: ["Python", "MySQL", "Pandas"],
+    link: "https://github.com/RaiyanAfiz/TorontoOpenData",
+  },
+];
