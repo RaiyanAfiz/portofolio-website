833dc00477636bbcfb2b63d50caf6d95ce312edc
Raiyan Afiz <raiyan.afiz@gmail.com>
2024-12-27 23:20:50 -0500

    Rename Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import About from "./components/About";
+import About from "./components/About/About";
 import Contact from "./components/Contact";
-import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects";
 
 const App = () => {
   return (
     <div>
-      <Hero />
       <About />
       <Projects />
       <Contact />
