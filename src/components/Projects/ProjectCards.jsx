087491cf784146ce2c611ba4ef302fb15975d8fc
Raiyan Afiz <raiyan.afiz@gmail.com>
2025-03-12 17:43:39 -0400

    Reworks projects section

---
+++ src/components/Projects/ProjectCards.jsx
@@ -0,0 +1,29 @@
+const ProjectCards = ({ projectProp }) => {
+  return (
+    <div className="project">
+      <h1 id="p-title">
+        <a href={projectProp.link}>{projectProp.title}</a>
+      </h1>
+
+      <img
+        id="p-img"
+        src={"src/assets/project-img/" + projectProp.picture}
+        alt={projectProp.pictureAlt}
+      ></img>
+
+      <div id="p-tag">
+        {projectProp.skills.map((tag) => {
+          return (
+            <button id="p-individual-tag" key={tag}>
+              {tag}
+            </button>
+          );
+        })}
+      </div>
+
+      <p id="p-description">{projectProp.description}</p>
+    </div>
+  );
+};
+
+export default ProjectCards;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,39 +1,16 @@
 import { myProjects } from "../../assets/ProjectData";
+import ProjectCards from "./ProjectCards";
 import "./Projects.css";
 
 const Projects = () => {
   return (
     <div>
-      {myProjects.reverse().map((project) => (
-        <div className="project" key={project.id}>
-          <br />
-          <div id="project-styles">
-            <h1 id="project-title">
-              <a href={project.link}>{project.title}</a>
-            </h1>
-
-            <img
-              id="project-picture"
-              src={"src/assets/project-img/" + project.picture}
-              alt={project.pictureAlt}
-            ></img>
-            <br />
-
-            <span id="project-skills">
-              {project.skills.map((tag) => {
-                return (
-                  <div id="skills-2" key={tag}>
-                    {tag}
-                  </div>
-                );
-              })}
-            </span>
-
-            <p id="project-description">{project.description}</p>
-          </div>
-          <br />
-        </div>
-      ))}
+      <h1>My Projects</h1>
+      <div className="projectsCards">
+        {myProjects.reverse().map((project) => (
+          <ProjectCards projectProp={project} />
+        ))}
+      </div>
     </div>
   );
 };
